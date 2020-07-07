76e356e5bfd61b422863904d870cb5bd5ffdf81c
Ashwini Abhishek <ashwiniabhishek10@gmail.com>
2020-07-07 17:36:27 +0530

    added some basic dummy test cases using jUnit 5

--- src/test/java/ashwini/abhishek/courses/CourseApplicationTests.java
+++ src/test/java/ashwini/abhishek/courses/CourseApplicationTests.java
@@ -1,9 +1,7 @@
 package ashwini.abhishek.courses;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class CourseApplicationTests {
 
 	@Test
---
+++ src/test/java/ashwini/abhishek/courses/TopicServiceTests.java
@@ -0,0 +1,14 @@
+package ashwini.abhishek.courses;
+
+import org.junit.jupiter.api.Test;
+
+public class TopicServiceTests {
+    @Test
+    void test() {
+        System.out.println("ashwin abhishek");
+    }
+    @Test
+    void test2() {
+        System.out.println("hello");
+    }
+}
